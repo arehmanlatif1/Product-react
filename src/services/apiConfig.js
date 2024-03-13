acf3ef798faafe8e19f317ba2f14b3937e0e3a45
arehmanlatif1 <rehmanlatif335@gmail.com>
2024-03-13 12:28:37 -0400

    update deployed API link

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -4,7 +4,7 @@ let apiUrl;
 
 const apiUrls = {
 
-  prodution: "www.products-react.com/api",
+  prodution: "https://nameless-sierra-38368-0a7a1bc0fdca.herokuapp.com/api",
   development: "http://localhost:3000",
 };
 
